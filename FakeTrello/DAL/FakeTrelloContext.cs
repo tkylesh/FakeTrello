8fc0ccd67ba67b277462128092e02f28a656ad5a
Taylor Shuff <tks2n@mtmail.mtsu.edu>
2017-03-04 12:15:09 -0600

    added dbcontext.

---
+++ FakeTrello/DAL/FakeTrelloContext.cs
@@ -0,0 +1,14 @@
+﻿using FakeTrello.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.DAL
+{
+    public class FakeTrelloContext : ApplicationDbContext
+    {
+        public virtual DbSet<TrelloUser> TrelloUsers { get; set; }
+    }
+}

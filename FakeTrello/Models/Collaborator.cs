94bd42b7d923330c38175f5b655520d3f4eb2ced
Taylor Shuff <tks2n@mtmail.mtsu.edu>
2017-03-11 14:01:29 -0600

    mar11 end of class saturday. repository stuff.

---
+++ FakeTrello/Models/Collaborator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.Models
+{
+    public class Collaborator
+    {
+        //manual or "explicit"? join table
+        //what we lose with this method is navigation properties
+        //these are not navigation properties
+        [Key]
+        public int CollaboratorId { get; set; }
+        [Required]
+        public string ApplicationUserId { get; set; }
+        [Required]
+        public int CardId { get; set; }
+
+    }
+}
